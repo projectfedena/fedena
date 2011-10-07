75150548654b10e78b1924a16049e7c5da468a39
foradian <foradian@gmail.com>
2011-10-07 12:27:06 +0000

    merge [plugin_base] revision=> 851 to 857
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@860 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- lib/fedena_plugin.rb
+++ lib/fedena_plugin.rb
@@ -1,12 +1,21 @@
 class FedenaPlugin
 
   AVAILABLE_MODULES = []
+  ADDITIONAL_LINKS = Hash.new{|k,v| k[v] = []}
+  REGISTERED_HOOKS = Hash.new{|k,v| k[v] = []}
+  FINANCE_CATEGORY = []
 
   def self.register=(plugin_details)
     unless AVAILABLE_MODULES.collect{|mod| mod[:name]}.include?(plugin_details[:name])
       AVAILABLE_MODULES << plugin_details
+      ADDITIONAL_LINKS[:student_profile_more_menu] << plugin_details[:student_profile_more_menu] unless plugin_details[:student_profile_more_menu].blank?
+      FINANCE_CATEGORY << plugin_details[:finance] unless plugin_details[:finance][:category_name].blank?
       Authorization::AUTH_DSL_FILES << "#{RAILS_ROOT}/vendor/plugins/#{plugin_details[:name]}/#{plugin_details[:auth_file]}"
+      if defined? plugin_details[:name].camelize.constantize
+        if plugin_details[:name].camelize.constantize.respond_to? "student_profile_hook"
+          REGISTERED_HOOKS[:student_profile] << plugin_details[:name]
+        end
+      end
     end
   end
 end
-
