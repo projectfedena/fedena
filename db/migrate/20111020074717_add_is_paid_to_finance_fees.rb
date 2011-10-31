d8a63220652c8d98a28256f31cd62644739a1822
foradian <foradian@gmail.com>
2011-10-31 06:59:20 +0000

    bug fixed=> display of fee pay due on calender in student login after payment his/her fees , fixes #915
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1078 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- db/migrate/20111020074717_add_is_paid_to_finance_fees.rb
+++ db/migrate/20111020074717_add_is_paid_to_finance_fees.rb
@@ -1,6 +1,6 @@
 class AddIsPaidToFinanceFees < ActiveRecord::Migration
   def self.up
-    add_column :finance_fees, :is_paid, :boolean, :default => false
+    add_column :finance_fees, :is_paid, :boolean, :default => 0
   end
 
   def self.down
