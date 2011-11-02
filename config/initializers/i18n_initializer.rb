c6d07a0a21e89528c8222ee3685119741fabfbf4
foradian <foradian@gmail.com>
2011-11-02 05:28:08 +0000

    feature => RTL conversion
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1112 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- config/initializers/i18n_initializer.rb
+++ config/initializers/i18n_initializer.rb
@@ -1,7 +1,7 @@
 if File.exists?(File.expand_path("./config/locale_settings.yml"))
   locale_settings = YAML::load(File.open(File.expand_path("./config/locale_settings.yml")))
   AVAILABLE_LANGUAGES = (locale_settings['available'].length > 0) ? locale_settings['available'] : { :en => 'English' }
-  DEFAULT_LANGUAGE = "en"
+  DEFAULT_LANGUAGE = (AVAILABLE_LANGUAGES.include?(locale_settings['default'])) ? locale_settings['default'] : AVAILABLE_LANGUAGES.keys[0].to_s
   AVAILABLE_LANGUAGE_CODES = locale_settings['available'].keys.map { |v| v.to_s }
   LANGUAGE_CODES_MAP = locale_settings['fallbacks']
   RTL_LANGUAGES = locale_settings['rtl']
