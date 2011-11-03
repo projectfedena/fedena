32a57ee04137e81377a57a1432e30e8f932264d3
foradian <foradian@gmail.com>
2011-11-03 06:44:40 +0000

    date translation in hindi on selection
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1127 1b8ff446-76f8-11e0-a33c-ba5893e5f458

---
+++ public/javascripts/calendar_date_select/locale/hi.js
@@ -0,0 +1,11 @@
+Date.weekdays = $w("Mo Tu We Th Fr Sa Su");
+Date.months = $w("जनवरी फ़रवरी मार्च अप्रैल मई जून जुलाई अगस्त सितम्बर अक्टूबर नवम्बर दिसम्बर" );
+
+Date.first_day_of_week = 1;
+
+_translations = {
+  "OK": "ओके",
+  "Now": "अब",
+  "Clear": "स्पष्ट",
+  "Today": "आज"
+}
