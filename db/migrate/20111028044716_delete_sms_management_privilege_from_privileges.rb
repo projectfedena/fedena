76407e7b3f51d25382d6fc251de97162ea703c00
foradian <foradian@gmail.com>
2011-11-03 09:07:14 +0000

    removed unwanted migration
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1129 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- db/migrate/20111028044716_delete_sms_management_privilege_from_privileges.rb
+++
@@ -1,9 +0,0 @@
-class DeleteSmsManagementPrivilegeFromPrivileges < ActiveRecord::Migration
-  def self.up
-    @privilege = Privilege.find_by_name("SMSManagement")
-    @privilege.destroy
-  end
-
-  def self.down
-  end
-end
