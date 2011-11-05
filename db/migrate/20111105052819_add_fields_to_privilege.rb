6770aea1e9f0447d34707d96d554f3a632a90a46
foradian <foradian@gmail.com>
2011-11-05 09:19:35 +0000

    changed privilege description texts
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1183 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- db/migrate/20111105052819_add_fields_to_privilege.rb
+++ db/migrate/20111105052819_add_fields_to_privilege.rb
@@ -2,7 +2,7 @@ class AddFieldsToPrivilege < ActiveRecord::Migration
   def self.up
     add_column :privileges, :description, :text
     Privilege.all.each do |privilege|
-      privilege.update_attributes(:description=>privilege.name.underscore)
+      privilege.update_attributes(:description=> privilege.name.underscore+"_privilege")
     end
   end
 
