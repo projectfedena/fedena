f2edd966c53c8fa5a182ab9c12f70bae04183a26
foradian <foradian@gmail.com>
2011-11-12 08:38:52 +0000

    bug fixed => bug in adding default value to description field of privilege table from migration
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1258 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- db/migrate/20111105052819_add_fields_to_privilege.rb
+++ db/migrate/20111105052819_add_fields_to_privilege.rb
@@ -2,7 +2,7 @@ class AddFieldsToPrivilege < ActiveRecord::Migration
   def self.up
     add_column :privileges, :description, :text
     Privilege.all.each do |privilege|
-      privilege.update_attributes(:description=> privilege.name.underscore+"_privilege")
+      execute("UPDATE privileges SET `description`='#{privilege.name.underscore+"_privilege"}' WHERE `name` = '#{privilege.name}'")
     end
   end
 
