36e99950c5e33ca50ecaad7e6a23bf328d1f10ef
foradian <foradian@gmail.com>
2011-11-14 09:20:23 +0000

    bug leave reset #1205 #1203
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1269 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/hr/employee_leave_type.rb
+++ app/models/hr/employee_leave_type.rb
@@ -21,6 +21,6 @@ class EmployeeLeaveType < ActiveRecord::Base
     has_many :employee_attendances
 
     validates_presence_of :name, :code
-    validates_uniqueness_of :name, :code
-    validates_format_of :max_leave_count, :with => /^[0-9]*\z/
+    validates_uniqueness_of :name, :code,:max_leave_count
+    validates_numericality_of :max_leave_count, :greater_than => 0
 end
