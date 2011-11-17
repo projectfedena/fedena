50f59826f866998d03ed93b9d10b0ace6e4081ff
foradian <foradian@gmail.com>
2011-11-17 08:30:21 +0000

    added association
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@1315 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/finance/finance_fee.rb
+++ app/models/finance/finance_fee.rb
@@ -17,8 +17,8 @@
 #limitations under the License.
 
 class FinanceFee < ActiveRecord::Base
-
-  belongs_to :finance_fee_collection
+  
+  belongs_to :finance_fee_collection ,:foreign_key => 'fee_collection_id'
   has_many   :finance_transactions ,:as=>:finance
   has_many :components, :class_name => 'FinanceFeeComponent', :foreign_key => 'fee_id'
   belongs_to :student
