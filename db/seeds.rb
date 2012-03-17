a1c82ecf1a2a049a183818a98d49c5bc2a28d9ea
foradian <foradian@gmail.com>
2012-03-17 08:02:20 +0000

    Add description to privilleges
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@2344 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,26 +45,26 @@ FinanceTransactionCategory.create(:name => 'Donation', :description => ' ', :is_
 FinanceTransactionCategory.create(:name => 'Fee', :description => ' ', :is_income => true)
 
 
-Privilege.create :name => "ExaminationControl"
-Privilege.create :name => "EnterResults"
-Privilege.create :name => "ViewResults"
-Privilege.create :name => "Admission"
-Privilege.create :name => "StudentsControl"
-Privilege.create :name => "ManageNews"
-Privilege.create :name => "ManageTimetable"
-Privilege.create :name => "StudentAttendanceView"
-Privilege.create :name => "HrBasics"
-Privilege.create :name => "AddNewBatch"
-Privilege.create :name => "SubjectMaster"
-Privilege.create :name => "EventManagement"
-Privilege.create :name => "GeneralSettings"
-Privilege.create :name => "FinanceControl"
-Privilege.create :name => "TimetableView"
-Privilege.create :name => "StudentAttendanceRegister"
-Privilege.create :name => "EmployeeAttendance"
-Privilege.create :name => "PayslipPowers"
-Privilege.create :name => "EmployeeSearch"
-Privilege.create :name => "SMSManagement"
+Privilege.create :name => 'ExaminationControl' , :description => 'Examination Control'
+Privilege.create :name => 'EnterResults' , :description => 'Enter Results'
+Privilege.create :name => 'ViewResults' , :description => 'View Results'
+Privilege.create :name => 'Admission' , :description => 'Admission'
+Privilege.create :name => 'StudentsControl' , :description => 'Students Control'
+Privilege.create :name => 'ManageNews' , :description => 'Manage News'
+Privilege.create :name => 'ManageTimetable' , :description => 'Manage Timetable'
+Privilege.create :name => 'StudentAttendanceView' , :description => 'Student Attendance View'
+Privilege.create :name => 'HrBasics' , :description => 'Hr Basics'
+Privilege.create :name => 'AddNewBatch' , :description => 'Add New Batch'
+Privilege.create :name => 'SubjectMaster' , :description => 'Subject Master'
+Privilege.create :name => 'EventManagement' , :description => 'Event Management'
+Privilege.create :name => 'GeneralSettings' , :description => 'General Settings'
+Privilege.create :name => 'FinanceControl' , :description => 'Finance Control'
+Privilege.create :name => 'TimetableView' , :description => 'Timetable View'
+Privilege.create :name => 'StudentAttendanceRegister' , :description => 'Student Attendance Register'
+Privilege.create :name => 'EmployeeAttendance' , :description => 'Employee Attendance'
+Privilege.create :name => 'PayslipPowers' , :description => 'Payslip Powers'
+Privilege.create :name => 'EmployeeSearch' , :description => 'Employee Search'
+Privilege.create :name => 'SMSManagement' , :description => 'Sms Management'
 
 
 Weekday.create :batch_id=>"", :weekday=>"1"
