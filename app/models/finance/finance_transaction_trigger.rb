b9335edac5ea6d740f80baf62ec54ddd22a842c3
foradian <foradian@gmail.com>
2012-03-26 13:34:49 +0000

    fixes #2153
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@2429 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/finance/finance_transaction_trigger.rb
+++ app/models/finance/finance_transaction_trigger.rb
@@ -19,5 +19,5 @@
 class FinanceTransactionTrigger < ActiveRecord::Base
   belongs_to :finance_category, :class_name => "FinanceTransactionCategory", :foreign_key => 'finance_category_id'
   validates_presence_of :percentage
-  validates_numericality_of :percentage,:less_than => 100
+  validates_numericality_of :percentage,:less_than => 100,:greater_than => 0
 end
