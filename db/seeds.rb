ac5e50b82ff4f910e5b5ff1c9e8e183e070c5889
foradian <foradian@gmail.com>
2012-03-28 09:07:22 +0000

    seed data for 2.3
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@2445 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,69 +1,95 @@
-Configuration.create :config_key => "InstitutionName", :config_value => ""
-Configuration.create :config_key => "InstitutionAddress", :config_value => ""
-Configuration.create :config_key => "InstitutionPhoneNo", :config_value => ""
-Configuration.create :config_key => "StudentAttendanceType", :config_value => "Daily"
-Configuration.create :config_key => "CurrencyType", :config_value => "$"
-Configuration.create :config_key => "Locale", :config_value=>"en"
-Configuration.create :config_key => "AdmissionNumberAutoIncrement", :config_value => "1"
-Configuration.create :config_key => "EmployeeNumberAutoIncrement", :config_value => "1"
-Configuration.create :config_key => "TotalSmsCount", :config_value=>"0"
-Configuration.create :config_key => "AvailableModules", :config_value=>"HR"
-Configuration.create :config_key => "AvailableModules", :config_value=>"Finance"
+[
+  {"config_key" => "InstitutionName"                 ,"config_value" => "" },
+  {"config_key" => "InstitutionAddress"              ,"config_value" => ""},
+  {"config_key" => "InstitutionPhoneNo"              ,"config_value" => ""},
+  {"config_key" => "StudentAttendanceType"           ,"config_value" => "Daily"},
+  {"config_key" => "CurrencyType"                    ,"config_value" => "$"},
+  {"config_key" => "Locale"                          ,"config_value" => "en"},
+  {"config_key" => "AdmissionNumberAutoIncrement"    ,"config_value" => "1"},
+  {"config_key" => "EmployeeNumberAutoIncrement"     ,"config_value" => "1"},
+  {"config_key" => "TotalSmsCount"                   ,"config_value" => "0"},
+  {"config_key" => "AvailableModules"                ,"config_value" => "HR"},
+  {"config_key" => "AvailableModules"                ,"config_value" => "Finance"},
+  {"config_key" => "NetworkState"                    ,"config_value" => "Online"},
+  {"config_key" => "FinancialYearStartDate"          ,"config_value" => Date.today},
+  {"config_key" => "FinancialYearEndDate"            ,"config_value" => Date.today+1.year},
+  {"config_key" => "AutomaticLeaveReset"             ,"config_value" => "0"},
+  {"config_key" => "LeaveResetPeriod"                ,"config_value" => "4"},
+  {"config_key" => "LastAutoLeaveReset"              ,"config_value" => nil}
+].each do |param|
+  Configuration.find_or_create_by_config_key_and_config_value(param)
+end
 
-GradingLevel.create(:name   => 'A',:min_score => '90')
-GradingLevel.create(:name   => 'B',:min_score => '80')
-GradingLevel.create(:name   => 'C',:min_score => '70')
-GradingLevel.create(:name   => 'D',:min_score => '60')
-GradingLevel.create(:name   => 'E',:min_score => '50')
-GradingLevel.create(:name   => 'F',:min_score => '0')
+
+[
+  {"name" => "A"   ,"min_score" => "" },
+  {"name" => "B"   ,"min_score" => ""},
+  {"name" => "C"   ,"min_score" => ""},
+  {"name" => "D"   ,"min_score" => "Daily"},
+  {"name" => "E"   ,"min_score" => "$"},
+  {"name" => "F"   ,"min_score" => "en"}
+].each do |param|
+  GradingLevel.find_or_create_by_name(param)
+end
+
+[
+  {"name" => "1"          ,"is_break" => false },
+  {"name" => "2"          ,"is_break" => false},
+  {"name" => "Interval"   ,"is_break" => true},
+  {"name" => "3"          ,"is_break" => false},
+  {"name" => "4"          ,"is_break" => false},
+  {"name" => "Lunch"      ,"is_break" => true},
+  {"name" => "5"          ,"is_break" => false},
+  {"name" => "6"          ,"is_break" => false},
+  {"name" => "7"          ,"is_break" => false}
+].each do |param|
+  ClassTiming.find_or_create_by_name(param)
+end
 
 
-ClassTiming.create(:name => "1",        :is_break => false)
-ClassTiming.create(:name => "2",        :is_break => false)
-ClassTiming.create(:name => "Interval", :is_break => true)
-ClassTiming.create(:name => "3",        :is_break => false)
-ClassTiming.create(:name => "4",        :is_break => false)
-ClassTiming.create(:name => "Lunch",    :is_break => true)
-ClassTiming.create(:name => "5",        :is_break => false)
-ClassTiming.create(:name => "6",        :is_break => false)
-ClassTiming.create(:name => "7",        :is_break => false)
+EmployeeCategory.find_or_create_by_name(:name => 'Fedena Admin',:prefix => 'Admin',:status => true)
 
-EmployeeCategory.create :name => 'Fedena Admin',:prefix => 'Admin',:status => true
+EmployeePosition.find_or_create_by_name(:name => 'Fedena Admin',:employee_category_id => 1,:status => true)
 
-EmployeePosition.create :name => 'Fedena Admin',:employee_category_id => 1,:status => true
+EmployeeDepartment.find_or_create_by_code(:code => 'Admin',:name => 'Fedena Admin',:status => true)
 
-EmployeeDepartment.create :code => 'Admin',:name => 'Fedena Admin',:status => true
+EmployeeGrade.find_or_create_by_name(:name => 'Fedena Admin',:priority => 0 ,:status => true,:max_hours_day=>nil,:max_hours_week=>nil)
 
-EmployeeGrade.create :name => 'Fedena Admin',:priority => 0 ,:status => true,:max_hours_day=>nil,:max_hours_week=>nil
+Employee.find_or_create_by_employee_number(:employee_number => 'admin',:joining_date => Date.today,:first_name => 'Fedena',:last_name => 'Administrator',
+  :employee_department_id => 1,:employee_grade_id => 1,:employee_position_id => 1,:employee_category_id => 1,:status => true,:nationality_id =>'76', :date_of_birth => Date.today-365)
 
-Employee.create :employee_number => 'admin',:joining_date => Date.today,:first_name => 'Fedena',:last_name => 'Administrator',
-:employee_department_id => 1,:employee_grade_id => 1,:employee_position_id => 1,:employee_category_id => 1,:status => true,:nationality_id =>'76', :date_of_birth => Date.today-365
 User.connection.execute "UPDATE users SET admin=1,employee=0 where id = 1"
 
-FinanceTransactionCategory.create(:name => 'Salary', :description => ' ', :is_income => false)
-FinanceTransactionCategory.create(:name => 'Donation', :description => ' ', :is_income => true)
-FinanceTransactionCategory.create(:name => 'Fee', :description => ' ', :is_income => true)
+[
+  {"name" => 'Salary'         ,"description" => ' ',"is_income" => false },
+  {"name" => 'Donation'       ,"description" => ' ',"is_income" => true},
+  {"name" => 'Fee'            ,"description" => ' ',"is_income" => true}
+].each do |param|
+  FinanceTransactionCategory.find_or_create_by_name(param)
+end
 
-Weekday.create :batch_id=>"", :weekday=>"1"
-Weekday.create :batch_id=>"", :weekday=>"2"
-Weekday.create :batch_id=>"", :weekday=>"3"
-Weekday.create :batch_id=>"", :weekday=>"4"
-Weekday.create :batch_id=>"", :weekday=>"5"
+[
+  {"batch_id" => ""          ,"weekday" => "1" },
+  {"batch_id" => ""          ,"weekday" => "2"},
+  {"batch_id" => ""          ,"weekday" => "3"},
+  {"batch_id" => ""          ,"weekday" => "4"},
+  {"batch_id" => ""          ,"weekday" => "5"},
+].each do |param|
+  Weekday.find_or_create_by_batch_id_and_weekday(param)
+end
 
-SmsSetting.create :settings_key=>"ApplicationEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"ParentSmsEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"EmployeeSmsEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"StudentSmsEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"ResultPublishEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"StudentAdmissionEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"ExamScheduleResultEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"AttendanceEnabled",:is_enabled=>false
-SmsSetting.create :settings_key=>"NewsEventsEnabled",:is_enabled=>false
+[
+  {"settings_key" => "ApplicationEnabled"                 ,"is_enabled" => false },
+  {"settings_key" => "ParentSmsEnabled"                   ,"is_enabled" => false},
+  {"settings_key" => "EmployeeSmsEnabled"                 ,"is_enabled" => false},
+  {"settings_key" => "StudentSmsEnabled"                  ,"is_enabled" => false},
+  {"settings_key" => "ResultPublishEnabled"               ,"is_enabled" => false},
+  {"settings_key" => "StudentAdmissionEnabled"            ,"is_enabled" => false},
+  {"settings_key" => "ExamScheduleResultEnabled"          ,"is_enabled" => false},
+  {"settings_key" => "AttendanceEnabled"                  ,"is_enabled" => false},
+  {"settings_key" => "NewsEventsEnabled"                  ,"is_enabled" => false},
+].each do |param|
+  SmsSetting.find_or_create_by_settings_key(param)
+end
 
-Configuration.create :config_key => "NetworkState", :config_value=>"Online"
-Configuration.create :config_key => "FinancialYearStartDate", :config_value=>Date.today
-Configuration.create :config_key => "FinancialYearEndDate", :config_value=>Date.today+1.year
-Configuration.create :config_key => "AutomaticLeaveReset", :config_value => "0"
-Configuration.create :config_key => "LeaveResetPeriod", :config_value => "4"
-Configuration.create :config_key => "LastAutoLeaveReset", :config_value => nil
 
