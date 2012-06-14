597a620978f8cc93b4dc7837c51fcd634ec4b329
foradian <foradian@gmail.com>
2012-06-14 11:37:48 +0000

    translation issue in employee controller(353)
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3038 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -350,7 +350,7 @@ class EmployeeController < ApplicationController
       sms_setting = SmsSetting.new()
       if sms_setting.application_sms_active and sms_setting.employee_sms_active
         recipient = ["#{@employee.mobile_phone}"]
-        message = "#{t('joinning_info')} #{@employee.first_name}. #{t('username')}: #{@employee.employee_number}, #{t('password')}: #{@employee.employee_number}123. #{t('change_password_after_login')}"
+        message = "#{t('joining_info')} #{@employee.first_name}. #{t('username')}: #{@employee.employee_number}, #{t('password')}: #{@employee.employee_number}123. #{t('change_password_after_login')}"
         Delayed::Job.enqueue(SmsManager.new(message,recipient))
       end
       flash[:notice] = "#{t('flash20')} #{ @employee.first_name}"
