9174f8fde3d82c07bd7e8c7c25624fe80281f5da
foradian <foradian@gmail.com>
2012-06-21 10:15:34 +0000

    javascript scripts added for searching
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3118 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -298,7 +298,7 @@ class UserController < ApplicationController
         :conditions => "(first_name LIKE \"#{params[:query]}%\"
                        OR last_name LIKE \"#{params[:query]}%\"
                        OR (concat(first_name, \" \", last_name) LIKE \"#{params[:query]}%\")
-                       OR username LIKE \"#{params[:query]}%\")",
+                       OR username LIKE  \"#{params[:query]}\")",
         :order => "first_name asc") unless params[:query] == ''
       #      else
       #        @user = User.first_name_or_last_name_or_username_equals params[:query].split
