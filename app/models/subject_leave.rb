3dbcf74d8f35b4182c4e2332d27f533aa9d135c0
foradian <foradian@gmail.com>
2012-06-22 06:49:19 +0000

    [FIXES BUG#3111]  can mark attendance before joining date for student
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3124 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/subject_leave.rb
+++ app/models/subject_leave.rb
@@ -4,4 +4,7 @@ class SubjectLeave < ActiveRecord::Base
   named_scope :by_month_and_subject, lambda { |d,s| { :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month , :subject_id => s} } }
   named_scope :by_month_batch_subject, lambda { |d,b,s| {  :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month , :subject_id => s,:batch_id=>b} } }
 
+  def validate
+    errors.add("#{t('attendance_before_the_date_of_admission')}")  if self.month_date < self.student.admission_date
+  end
 end
