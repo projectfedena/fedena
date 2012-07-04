c26c92f60003675a2614a5540dc932f405be7c61
foradian <foradian@gmail.com>
2012-07-04 11:42:21 +0000

    introduce scope for cce reports using observable_type instead of exam_id
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3257 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/cce_report.rb
+++ app/models/cce_report.rb
@@ -5,4 +5,7 @@ class CceReport < ActiveRecord::Base
 #  has_and_belongs_to_many   :exams
   belongs_to    :observable, :polymorphic=>true
   belongs_to    :exam
+
+  named_scope :scholastic,{:conditions=>{:observable_type=>"FaCriteria"}}
+  named_scope :coscholastic,{:conditions=>{:observable_type=>"Observation"}}
 end
