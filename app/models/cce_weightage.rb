03d92aca3b142ce44c44788c4a269dbb0c93cad9
foradian <foradian@gmail.com>
2012-07-10 10:33:50 +0000

    [FIXES BUG #3239] change Cce to "CCE" in flash message of "Cce exam category can't be blank"
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3363 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/cce_weightage.rb
+++ app/models/cce_weightage.rb
@@ -1,5 +1,8 @@
 class CceWeightage < ActiveRecord::Base
   has_and_belongs_to_many :courses
   belongs_to              :cce_exam_category
-  validates_presence_of :weightage,:criteria_type,:cce_exam_category_id
+  validates_presence_of :weightage,:criteria_type
+  def validate
+    errors.add_to_base("CCE Exam category can't be blank") if self.cce_exam_category_id.blank?
+  end
 end
