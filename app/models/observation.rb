1036c23829dc7338c5ccf0bdfaf44633c1a91a84
foradian <foradian@gmail.com>
2012-07-10 05:28:01 +0000

    [FIXES BUG #3246]  500in Co Scholastic Group --Co Scholastic Criterias--click on save without adding any details
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3342 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -5,12 +5,16 @@ class Observation < ActiveRecord::Base
   accepts_nested_attributes_for :descriptive_indicators
   has_many    :cce_reports, :as=>:observable
 
-  validates_presence_of :name,  :desc
 
   def next_record
     observation_group.observations.first(:conditions => ['order > ?',order])
   end
   def prev_record
     observation_group.observations.last(:conditions => ['order < ?',order])
+  end
+
+  def validate
+    errors.add_to_base("Name can't be blank") if self.name.blank?
+    errors.add_to_base("Description can't be blank") if self.desc.blank? 
   end
 end
