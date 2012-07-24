d6fa6b17efd6bd9f39ab7ae407da14a6ad4413bb
foradian <foradian@gmail.com>
2012-07-24 06:41:20 +0000

    additional fields validation to allow alphanumeric or alphabetic only
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3451 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/additional_field.rb
+++ app/models/additional_field.rb
@@ -18,6 +18,6 @@
 
 class AdditionalField < ActiveRecord::Base
   validates_presence_of :name
-  validates_format_of     :name, :with => /^[A-Z0-9 ]*$/i,
+  validates_format_of     :name, :with => /^[a-z ][a-z0-9 ]*$/i,
     :message => "#{t('must_contain_only_letters_numbers_space')}"
 end
