804219d50a8d460a7addf6f43647cfe4bb1a0168
foradian <foradian@gmail.com>
2012-08-02 05:46:02 +0000

    multiple validations for subject leave
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3482 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/subject_leave.rb
+++ app/models/subject_leave.rb
@@ -1,11 +1,13 @@
 class SubjectLeave < ActiveRecord::Base
   belongs_to :student
   belongs_to :batch
+  validates_presence_of :subject_id
+  validates_presence_of :batch_id
+  validates_presence_of :student_id
+  validates_presence_of :month_date
   named_scope :by_month_and_subject, lambda { |d,s| { :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month , :subject_id => s} } }
   named_scope :by_month_batch_subject, lambda { |d,b,s| {  :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month , :subject_id => s,:batch_id=>b} } }
-  def validate
-    errors.add("#{t('attendance_before_the_date_of_admission')}")  if self.month_date < self.student.admission_date
-  end
+  validates_uniqueness_of :student_id,:scope=>[:class_timing_id,:month_date]
   def validate
     errors.add("#{t('attendance_before_the_date_of_admission')}")  if self.month_date < self.student.admission_date
   end
