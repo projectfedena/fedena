7e5458c6b9f1934cdc297fee260afff5440a3841
foradian <foradian@gmail.com>
2012-08-06 09:27:57 +0000

    reason validation for subject leave
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3489 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/subject_leave.rb
+++ app/models/subject_leave.rb
@@ -5,6 +5,7 @@ class SubjectLeave < ActiveRecord::Base
   validates_presence_of :batch_id
   validates_presence_of :student_id
   validates_presence_of :month_date
+  validates_presence_of :reason
   named_scope :by_month_and_subject, lambda { |d,s| { :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month , :subject_id => s} } }
   named_scope :by_month_batch_subject, lambda { |d,b,s| {  :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month , :subject_id => s,:batch_id=>b} } }
   validates_uniqueness_of :student_id,:scope=>[:class_timing_id,:month_date]
