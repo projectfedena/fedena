af5acbb6818d2ba8dc06a80ea91854cb783c267c
foradian <foradian@gmail.com>
2012-09-15 11:26:43 +0000

    fixed 500 issue in employee payslip pdf [fixed #3551]
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3702 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1424,6 +1424,10 @@ class EmployeeController < ApplicationController
   end
   def employee_individual_payslip_pdf
     @employee = Employee.find(:first,:conditions=>"id=#{params[:id]}")
+    if @employee.blank?
+      @employee = ArchivedEmployee.find(:first,:conditions=>"former_id=#{params[:id]}")
+      @employee.id = @employee.former_id
+    end
     @bank_details = EmployeeBankDetail.find_all_by_employee_id(@employee.id)
     @employee ||= ArchivedEmployee.find(:first,:conditions=>"former_id=#{params[:id]}")
     @department = EmployeeDepartment.find(@employee.employee_department_id).name
