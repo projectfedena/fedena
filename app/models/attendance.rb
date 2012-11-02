948eb157e8d0cdb26ad9122ee0043a6e6315de2b
foradian <foradian@gmail.com>
2012-11-02 06:03:12 +0000

    CODE REVIEW #3689 fixed
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3920 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -17,14 +17,13 @@
 #limitations under the License.
 
 class Attendance < ActiveRecord::Base
-  belongs_to :subject
   belongs_to :student
   belongs_to :batch
   validates_presence_of :reason
   validates_uniqueness_of :student_id, :scope => [:month_date],:message=>"already marked as absent"
 
   def validate
-    errors.add("#{t('attendance_before_the_date_of_admission')}")  if self.month_date < self.student.admission_date
+    errors.add("#{t('attendance_before_the_date_of_admission')}")  if self.student.present? and self.month_date < self.student.admission_date
   end
   named_scope :by_month, lambda { |d| { :conditions  => { :month_date  => d.beginning_of_month..d.end_of_month } } }
   named_scope :by_month_and_batch, lambda { |d,b| {:conditions  => { :month_date  => d.beginning_of_month..d.end_of_month,:batch_id=>b } } }
