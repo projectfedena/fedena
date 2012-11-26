bb4d696ef888a1ebf301804d2fef6005cfaf1922
foradian <foradian@gmail.com>
2012-11-26 05:54:22 +0000

    student validation place in attendance model
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@3971 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -19,7 +19,8 @@
 class Attendance < ActiveRecord::Base
   belongs_to :student
   belongs_to :batch
-  validates_presence_of :reason,:month_date,:batch_id
+
+  validates_presence_of :reason,:month_date,:batch_id,:student_id
   validates_uniqueness_of :student_id, :scope => [:month_date],:message=>"already marked as absent"
 
   def after_validate
