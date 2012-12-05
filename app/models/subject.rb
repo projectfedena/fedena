3c44760419ba8fbaa85e1571f65a3a4e32472f07
foradian <foradian@gmail.com>
2012-12-05 11:02:12 +0000

    amount can be added as nil whle creating subjects
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@4016 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -29,7 +29,8 @@ class Subject < ActiveRecord::Base
   has_and_belongs_to_many_with_deferred_save :fa_groups
   validates_presence_of :name, :max_weekly_classes, :code,:batch_id
   validates_presence_of :credit_hours, :if=>:check_grade_type
-  validates_numericality_of :max_weekly_classes,:amount
+  validates_numericality_of :max_weekly_classes
+  validates_numericality_of :amount,:allow_nil => true
   validates_uniqueness_of :code, :case_sensitive => false, :scope=>[:batch_id,:is_deleted] ,:if=> 'is_deleted == false'
   named_scope :for_batch, lambda { |b| { :conditions => { :batch_id => b.to_i, :is_deleted => false } } }
   named_scope :without_exams, :conditions => { :no_exams => false, :is_deleted => false }
