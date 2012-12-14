7e4e353c90a59a2b397b6862e3b8735570da5603
foradian <foradian@gmail.com>
2012-12-14 11:04:17 +0000

    uniqueness of code to be taken according to course
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@4045 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/subject_amount.rb
+++ app/models/subject_amount.rb
@@ -1,7 +1,7 @@
 class SubjectAmount < ActiveRecord::Base
   belongs_to :course
 
-  validates_uniqueness_of :code
+  validates_uniqueness_of :code,:scope => :course_id
   validates_presence_of :course_id,:amount,:code
   validates_numericality_of :amount
 end
