70efcafb1049efc29df4183288e588673ad3341b
foradian <foradian@gmail.com>
2012-12-28 11:45:41 +0000

    fixes #3831 [Place proper associations in every model] first commit
    git-svn-id: file:///svn/fedena2.1/branches/fedena2.1_i18n@4072 1b8ff446-76f8-11e0-a33c-ba5893e5f458

--- app/models/hr/employee.rb
+++ app/models/hr/employee.rb
@@ -22,10 +22,11 @@ class Employee < ActiveRecord::Base
   belongs_to  :employee_grade
   belongs_to  :employee_department
   belongs_to  :nationality, :class_name => 'Country'
-  belongs_to  :user, :dependent=>:destroy 
+  belongs_to  :user, :dependent=>:destroy
+  belongs_to  :reporting_manager,:class_name => "Employee"
   
-  has_many :employees_subjects
-  has_many :subjects ,:through => :employees_subjects
+  has_many    :employees_subjects
+  has_many    :subjects ,:through => :employees_subjects
   has_many    :timetable_entries
   has_many    :employee_bank_details
   has_many    :employee_additional_details
