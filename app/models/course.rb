d8c2395921010b2fa87ec304e55db2ed3ce3ab6c
Trung Lê <trung.le@ruby-journal.com>
2013-09-09 23:05:23 +1000

    Refactor Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -15,10 +15,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 class Course < ActiveRecord::Base
 
-  GRADINGTYPES = {"1"=>"GPA","2"=>"CWA","3"=>"CCE"}
+  GRADINGTYPES = { '1' => 'GPA', '2' => 'CWA', '3' => 'CCE' }
   INVERT_GRADINGTYPES = GRADINGTYPES.invert
 
   validates_presence_of :course_name, :code
@@ -35,13 +34,9 @@ class Course < ActiveRecord::Base
 
   before_save :cce_weightage_valid
 
-  named_scope :active, :conditions => { :is_deleted => false }, :order => 'course_name asc'
-  named_scope :deleted, :conditions => { :is_deleted => true }, :order => 'course_name asc'
-  named_scope :cce, {:select => "courses.*",:conditions=>{:grading_type => GRADINGTYPES.invert["CCE"]}, :order => 'course_name asc'}
-
-  def presence_of_initial_batch
-    errors.add_to_base "#{t('should_have_an_initial_batch')}" if batches.length == 0
-  end
+  named_scope :active,  :conditions => { :is_deleted => false }, :order => 'course_name ASC'
+  named_scope :deleted, :conditions => { :is_deleted => true },  :order => 'course_name ASC'
+  named_scope :cce, { :select => 'courses.*', :conditions => { :grading_type => GRADINGTYPES.invert['CCE'] }, :order => 'course_name ASC' }
 
   def inactivate
     update_attribute(:is_deleted, true)
@@ -52,7 +47,7 @@ class Course < ActiveRecord::Base
   end
 
   def active_batches
-    self.batches.all(:conditions=>{:is_active=>true,:is_deleted=>false})
+    self.batches.all(:conditions => { :is_active => true, :is_deleted => false })
   end
 
   def has_batch_groups_with_active_batches
@@ -61,43 +56,45 @@ class Course < ActiveRecord::Base
       return false
     else
       batch_groups.each do|b|
-        return true if b.has_active_batches==true
+        return true if b.has_active_batches
       end
     end
     return false
   end
 
-  def find_course_rank(batch_ids,sort_order)
-    batches = Batch.find_all_by_id(batch_ids)
+  def find_course_rank(batch_ids, sort_order)
+    batches   = Batch.find_all_by_id(batch_ids)
     @students = Student.find_all_by_batch_id(batches)
-    @grouped_exams = GroupedExam.find_all_by_batch_id(batches)
-    ordered_scores = []
-    student_scores = []
+    @grouped_exams  = GroupedExam.find_all_by_batch_id(batches)
+    ordered_scores  = []
+    student_scores  = []
     ranked_students = []
-    @students.each do|student|
-      score = GroupedExamReport.find_by_student_id_and_batch_id_and_score_type(student.id,student.batch_id,"c")
+
+    @students.each do |student|
+      score = GroupedExamReport.find_by_student_id_and_batch_id_and_score_type(student.id, student.batch_id, 'c')
       marks = 0
-      unless score.nil?
-        marks = score.marks
-      end
+      marks = score.marks if !score.nil?
       ordered_scores << marks
-      student_scores << [student.id,marks]
+      student_scores << [student.id, marks]
     end
+
     ordered_scores = ordered_scores.compact.uniq.sort.reverse
+
     @students.each do |student|
       m = 0
       student_scores.each do|student_score|
-        if student_score[0]==student.id
+        if student_score[0] == student.id
           m = student_score[1]
         end
       end
-      if sort_order=="" or sort_order=="rank-ascend" or sort_order=="rank-descend"
-        ranked_students << [(ordered_scores.index(m) + 1),m,student.id,student]
+      if sort_order == '' or sort_order == 'rank-ascend' or sort_order == 'rank-descend'
+        ranked_students << [(ordered_scores.index(m) + 1), m, student.id, student]
       else
-        ranked_students << [student.full_name,(ordered_scores.index(m) + 1),m,student.id,student]
+        ranked_students << [student.full_name, (ordered_scores.index(m) + 1), m, student.id, student]
       end
     end
-    if sort_order=="" or sort_order=="rank-ascend" or sort_order=="name-ascend"
+
+    if sort_order == '' or sort_order == 'rank-ascend' or sort_order == 'name-ascend'
       ranked_students = ranked_students.sort
     else
       ranked_students = ranked_students.sort.reverse
@@ -105,20 +102,21 @@ class Course < ActiveRecord::Base
   end
 
   def cce_enabled?
-    Configuration.cce_enabled? and grading_type == "3"
+    Configuration.cce_enabled? && grading_type == INVERT_GRADINGTYPES['CCE']
   end
 
   def gpa_enabled?
-    Configuration.has_gpa? and self.grading_type=="1"
+    Configuration.has_gpa? && grading_type == INVERT_GRADINGTYPES['GPA']
   end
 
   def cwa_enabled?
-    Configuration.has_cwa? and self.grading_type=="2"
+    Configuration.has_cwa? && grading_type == INVERT_GRADINGTYPES['CWA']
   end
 
   def normal_enabled?
-    self.grading_type.nil? or self.grading_type=="0"
+    self.grading_type.nil? || self.grading_type == '0'
   end
+
   #  def guardian_email_list
   #    email_addresses = []
   #    students = self.students
@@ -136,24 +134,28 @@ class Course < ActiveRecord::Base
   #    end
   #    email_addresses
   #  end
-  class << self
-    def grading_types
-      hsh =  ActiveSupport::OrderedHash.new
-      hsh["0"]="Normal"
-      types = Configuration.get_grading_types
-      types.each{|t| hsh[t] = GRADINGTYPES[t]}
-      hsh
-    end
-    def grading_types_as_options
-      grading_types.invert.sort_by{|k,v| v}
-    end
+
+  def self.grading_types
+    hsh =  ActiveSupport::OrderedHash.new
+    hsh["0"] = "Normal"
+    types = Configuration.get_grading_types
+    types.each { |t| hsh[t] = GRADINGTYPES[t] }
+    hsh
+  end
+
+  def self.grading_types_as_options
+    grading_types.invert.sort_by { |k, _| v }
   end
 
   def cce_weightages_for_exam_category(cce_exam_cateogry_id)
-    cce_weightages.all(:conditions=>{:cce_exam_category_id=>cce_exam_cateogry_id})
+    cce_weightages.all(:conditions => { :cce_exam_category_id => cce_exam_cateogry_id })
   end
 
   private
+
+  def presence_of_initial_batch
+    errors.add_to_base "#{t('should_have_an_initial_batch')}" if batches.length == 0
+  end
 
   def cce_weightage_valid
     cce_weightages.group_by(&:criteria_type).values.each do |v|
@@ -163,7 +165,6 @@ class Course < ActiveRecord::Base
       end
     end
     true
-
   end
 
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Course do
   before do
-    @course1 = Factory.create(:course, :is_deleted => false)
-    @course2 = Factory.create(:course, :is_deleted => true)
+    @course1 = FactoryGirl.create(:course, :is_deleted => false)
+    @course2 = FactoryGirl.create(:course, :is_deleted => true)
   end
 
   context 'validate course' do
@@ -17,8 +17,16 @@ describe Course do
     it { should have_and_belong_to_many(:observation_groups) }
 
     describe '#presence_of_initial_batch' do
-      it 'should at latest have a batch' do
-        @course1.batches.count.should >= 1
+      context 'no batches on create' do
+        before do
+          @course = FactoryGirl.build(:course)
+          @course.stub_chain(:batches, :length).and_return(0)
+        end
+
+        it 'adds error to base' do
+          @course.should be_invalid
+          @course.errors[:base].should == 'Should have an initial batch'
+        end
       end
     end
   end
@@ -32,104 +40,151 @@ describe Course do
 
   describe '#full_name' do
     it 'returns full name of course' do
-      @course = Course.new(:course_name => "1", :section_name => "A")
-      @course.full_name.should == "1 A"
+      @course = Course.new(:course_name => '1', :section_name => 'A')
+      @course.full_name.should == '1 A'
     end
   end
 
   describe '#active_batches' do
     before do
       @course = Course.new(:course_name => '1', :section_name => 'A', :code => '1A')
-      @batch1 = Factory.build(:batch, :is_active=>true,:is_deleted=>false)
-      @batch2 = Factory.build(:batch, :is_active=>true,:is_deleted=>true)
+      @batch1 = FactoryGirl.build(:batch, :is_active => true, :is_deleted => false)
+      @batch2 = FactoryGirl.build(:batch, :is_active => true, :is_deleted => true)
       @course.batches << [@batch1, @batch2]
       @course.save
     end
+
     it 'returns all active batches' do
       @course.active_batches.should == [@batch1]
     end
   end
 
   describe '#has_batch_groups_with_active_batches' do
-    it 'returns false' do
-      @course1.has_batch_groups_with_active_batches.should be_false
+    context 'no active batches' do
+      it 'returns false' do
+        @course1.has_batch_groups_with_active_batches.should be_false
+      end
     end
 
-    it 'returns true' do
-      @batch_groups = Factory.create(:batch_group, :course => @course1)
-      @batch_groups.batches = @course1.batches
-      @course1.has_batch_groups_with_active_batches.should be_true
+    context 'with active batches' do
+      before do
+        @batch_groups = FactoryGirl.create(:batch_group, :course => @course1)
+        @batch_groups.batches = @course1.batches
+      end
+
+      it 'returns true' do
+        @course1.has_batch_groups_with_active_batches.should be_true
+      end
     end
   end
 
   describe '#cce_enabled?' do
-    before { @course1.grading_type = Course::INVERT_GRADINGTYPES["CCE"] }
+    context 'cce is enabled in Configuration and grading type is CCE' do
+      before do
+        Configuration.stub(:cce_enabled?).and_return(true)
+        @course1.grading_type = Course::INVERT_GRADINGTYPES['CCE']
+      end
 
-    it 'returns true' do
-      Configuration.stub(:cce_enabled?).and_return(true)
-      @course1.should be_cce_enabled
+      it 'returns true' do
+        @course1.should be_cce_enabled
+      end
     end
 
-    it 'returns false' do
-      Configuration.stub(:cce_enabled?).and_return(false)
-      @course1.should_not be_cce_enabled
+    context 'cce is disabled in Configuration' do
+      before do
+        Configuration.stub(:cce_enabled?).and_return(false)
+      end
+
+      it 'returns false' do
+        @course1.should_not be_cce_enabled
+      end
+    end
+
+    context 'cce is enabled in Configuration and grading type is not CCE' do
+      before do
+        Configuration.stub(:cce_enabled?).and_return(true)
+        @course1.grading_type = Course::INVERT_GRADINGTYPES['GPA']
+      end
+
+      it 'returns false' do
+        @course1.should_not be_cce_enabled
+      end
     end
   end
 
   describe '#gpa_enabled?' do
-    before { @course1.grading_type = Course::INVERT_GRADINGTYPES["GPA"] }
+    context 'gpa is enabled in Configuration and grading type is GPA' do
+      before do
+        Configuration.stub(:has_gpa?).and_return(true)
+        @course1.grading_type = Course::INVERT_GRADINGTYPES['GPA']
+      end
 
-    it 'returns true' do
-      Configuration.stub(:has_gpa?).and_return(true)
-      @course1.should be_gpa_enabled
+      it 'returns true' do
+        @course1.should be_gpa_enabled
+      end
     end
 
-    it 'returns false' do
-      Configuration.stub(:has_gpa?).and_return(false)
-      @course1.should_not be_gpa_enabled
-    end
-  end
+    context 'gpa is disabled in Configuration' do
+      before do
+        Configuration.stub(:has_gpa?).and_return(false)
+      end
 
-  describe '#cwa_enabled?' do
-    before { @course1.grading_type = Course::INVERT_GRADINGTYPES["CWA"] }
-
-    it 'returns true' do
-      Configuration.stub(:has_cwa?).and_return(true)
-      @course1.should be_cwa_enabled
+      it 'returns false' do
+        @course1.should_not be_gpa_enabled
+      end
     end
 
-    it 'returns false' do
-      Configuration.stub(:has_cwa?).and_return(false)
-      @course1.should_not be_cwa_enabled
+    context 'gpa is enabled in Configuration and grading type is not GPA' do
+      before do
+        Configuration.stub(:has_gpa?).and_return(true)
+        @course1.grading_type = Course::INVERT_GRADINGTYPES['CCE']
+      end
+
+      it 'returns false' do
+        @course1.should_not be_gpa_enabled
+      end
     end
   end
 
   describe '#normal_enabled?' do
-    it 'returns true' do
-      @course1.grading_type = "0"
-      @course1.should be_normal_enabled
+    context 'grading type is nil' do
+      before do
+        @course1.grading_type = nil
+      end
+
+      it 'returns true' do
+        @course1.should be_normal_enabled
+      end
+    end
+
+    context 'grading type is 0' do
+      before do
+        @course1.grading_type = '0'
+      end
+
+      it 'returns true' do
+        @course1.should be_normal_enabled
+      end
     end
   end
 
-  describe "scope_name test" do
-    describe ".active" do
-      it "returns active course" do
-        Course.active.should == [@course1]
-      end
+  describe '.active' do
+    it 'returns active course' do
+      Course.active.should == [@course1]
     end
+  end
 
-    describe ".deleted" do
-      it "returns deleted course" do
-        Course.deleted.should == [@course2]
-      end
+  describe '.deleted' do
+    it 'returns deleted course' do
+      Course.deleted.should == [@course2]
     end
+  end
 
-    describe ".cce" do
-      it "returns CCE course" do
-        @course1.grading_type = Course::INVERT_GRADINGTYPES["CCE"]
-        @course1.save
-        Course.cce.should == [@course1]
-      end
+  describe '.cce' do
+    it 'returns CCE course' do
+      @course1.grading_type = Course::INVERT_GRADINGTYPES['CCE']
+      @course1.save
+      Course.cce.should == [@course1]
     end
   end
 
