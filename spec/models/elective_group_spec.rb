4a249ac2dea71f2a36df2123e554d3f351ac5e79
Trung Lê <trung.le@ruby-journal.com>
2013-09-12 09:40:42 +1000

    Refactor ElectiveGroup spec

--- spec/models/elective_group_spec.rb
+++ spec/models/elective_group_spec.rb
@@ -1,31 +1,29 @@
 require 'spec_helper'
 
 describe ElectiveGroup do
-  context "validate elective group" do
+
+  it { should belong_to(:batch) }
+  it { should have_many(:subjects) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:batch_id) }
+
+  describe '#inactive' do
+    let(:elective_group) { Factory.create(:elective_group) }
+
+    it 'must be disabled' do
+      elective_group.inactivate
+      elective_group.should be_is_deleted
+    end
+  end
+
+  describe '.for_batch' do
     before do
       @elective_group1 = Factory.create(:elective_group, :batch_id => 20, :is_deleted => false)
       @elective_group2 = Factory.create(:elective_group, :batch_id => 22, :is_deleted => true)
     end
 
-    it { should belong_to(:batch) }
-    it { should have_many(:subjects) }
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:batch_id) }
-
-    describe '#inactive' do
-      it 'must be disabled' do
-        @elective_group1.inactivate
-        @elective_group1.should be_is_deleted
-      end
+    it 'returns for_batch' do
+      ElectiveGroup.for_batch(20).should == [@elective_group1]
     end
-
-    describe "scope_name test" do
-      describe ".for_batch" do
-        it "returns for_batch" do
-          ElectiveGroup.for_batch(20).should == [@elective_group1]
-        end
-      end
-    end
-
   end
 end
