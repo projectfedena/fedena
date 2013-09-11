c2a2baa70c4d22ba54a5d8fa6d5ad69b424a3351
Quang Anh <hoquanganh.bmt@gmail.com>
2013-09-11 19:50:18 +0700

    Add test for Reminder model [Closes #28]

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 class Reminder < ActiveRecord::Base
   validates_presence_of :body
-  belongs_to :user , :foreign_key => 'sender'
+  belongs_to :user, :foreign_key => 'sender'
   belongs_to :to_user, :class_name => 'User', :foreign_key => 'recipient'
 
   cattr_reader :per_page
