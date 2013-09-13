362fa6724e119e8036ff83333909c46df5e59023
Rob Zolkos <robzolkos@gmail.com>
2013-09-13 23:28:51 +1000

    add spec for observations controller

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -19,9 +19,10 @@
 class ObservationsController < ApplicationController
   before_filter :login_required
   filter_access_to :all
+
   def show
-    @observation=Observation.find params[:id]
-    @descriptives=@observation.descriptive_indicators
+    @observation  = Observation.find params[:id]
+    @descriptives = @observation.descriptive_indicators
   end
 
 end
