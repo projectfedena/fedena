6ed757d28bfa3808fb496e8dcb66f42ecab1aff3
Toàn Lê <ktoanlba@gmail.com>
2013-09-22 10:45:03 +0700

    Write test StudentFeeCollectionDiscount [Closes #93]

--- app/models/finance/student_fee_collection_discount.rb
+++ app/models/finance/student_fee_collection_discount.rb
@@ -16,16 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 class StudentFeeCollectionDiscount < FeeCollectionDiscount
-   belongs_to :receiver ,:class_name=>'Student'
-
-  validates_presence_of  :receiver_id , :message => "#{t('student_admission_no_cant_be_blank')}"
-
-
+  belongs_to :receiver ,:class_name => 'Student'
+  validates_presence_of :receiver_id, :message => "#{t('student_admission_no_cant_be_blank')}"
 
   def student_name
-    s =Student.find(self.receiver_id)
+    s = Student.find(self.receiver_id)
     "#{s.first_name} (#{s.admission_no})" unless s.nil?
   end
 
