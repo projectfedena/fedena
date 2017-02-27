f86a49a283da1357a09ca3ae054f4f4b8003daaf
George Thomas <iamgeorgethomas@gmail.com>
2017-02-27 19:56:39 +0530

    Rails-3-upgrade: Use Rails 3 configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
-  
+Fedena::Application.routes.draw do
   map.resources :grading_levels
   map.resources :ranking_levels, :collection => {:create_ranking_level=>[:get,:post], :edit_ranking_level=>[:get,:post], :update_ranking_level=>[:get,:post], :delete_ranking_level=>[:get,:post], :ranking_level_cancel=>[:get,:post], :change_priority=>[:get,:post]}
   map.resources :class_designations
@@ -62,8 +61,7 @@ ActionController::Routing::Routes.draw do |map|
 
   #map.connect 'parts/:number', :controller => 'inventory', :action => 'sho
   map.connect ':controller/:action/:id'
-  map.connect ':controller/:action' 
+  map.connect ':controller/:action'
   map.connect ':controller/:action/:id/:id2'
   map.connect ':controller/:action/:id.:format'
-
 end
