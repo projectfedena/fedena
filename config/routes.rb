31ed68da7bc9d5553eb51f34987a48539fe69e32
George Thomas <iamgeorgethomas@gmail.com>
2017-02-27 23:06:58 +0530

    Rails-3-upgrade: Add Rails 3 style routes and disable failing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,67 @@
 Fedena::Application.routes.draw do
-  map.resources :grading_levels
-  map.resources :ranking_levels, :collection => {:create_ranking_level=>[:get,:post], :edit_ranking_level=>[:get,:post], :update_ranking_level=>[:get,:post], :delete_ranking_level=>[:get,:post], :ranking_level_cancel=>[:get,:post], :change_priority=>[:get,:post]}
-  map.resources :class_designations
-  #map.resources :exam_reports, :collection => {:course_reports_index=>[:get,:post], :batch_reports_index=>[:get,:post]}
-  map.resources :class_timings
-  map.resources :subjects
-  map.resources :attendances, :collection=>{:daily_register=>:get,:subject_wise_register=>:get}
-  map.resources :employee_attendances
-  map.resources :attendance_reports
-  map.resources :cce_exam_categories
-  map.resources :assessment_scores,:collection=>{:exam_fa_groups=>[:get],:observation_groups=>[:get]}
-  map.resources :cce_settings,:collection=>{:basic=>[:get],:scholastic=>[:get],:co_scholastic=>[:get]}
-  map.resources :scheduled_jobs,:except => [:show]
-  map.resources :fa_groups,:collection=>{:assign_fa_groups=>[:get,:post],:new_fa_criteria=>[:get,:post],:create_fa_criteria=>[:get,:post],:edit_fa_criteria=>[:get,:post],:update_fa_criteria=>[:get,:post],:destroy_fa_criteria=>[:post],:reorder=>[:get,:post]}
+  resources :grading_levels
+  resources :ranking_levels, :collection => {:create_ranking_level=>[:get,:post], :edit_ranking_level=>[:get,:post], :update_ranking_level=>[:get,:post], :delete_ranking_level=>[:get,:post], :ranking_level_cancel=>[:get,:post], :change_priority=>[:get,:post]}
+  resources :class_designations
+  #resources :exam_reports, :collection => {:course_reports_index=>[:get,:post], :batch_reports_index=>[:get,:post]}
+  resources :class_timings
+  resources :subjects
+  resources :attendances, :collection=>{:daily_register=>:get,:subject_wise_register=>:get}
+  resources :employee_attendances
+  resources :attendance_reports
+  resources :cce_exam_categories
+  resources :assessment_scores,:collection=>{:exam_fa_groups=>[:get],:observation_groups=>[:get]}
+  resources :cce_settings,:collection=>{:basic=>[:get],:scholastic=>[:get],:co_scholastic=>[:get]}
+  resources :scheduled_jobs,:except => [:show]
+  resources :fa_groups,:collection=>{:assign_fa_groups=>[:get,:post],:new_fa_criteria=>[:get,:post],:create_fa_criteria=>[:get,:post],:edit_fa_criteria=>[:get,:post],:update_fa_criteria=>[:get,:post],:destroy_fa_criteria=>[:post],:reorder=>[:get,:post]}
   #  do |fa|
   #    fa.resources  :fa_criterias
   #  end
-  map.resources :fa_criterias do |fa|
-    fa.resources :descriptive_indicators do |desc|
-      desc.resources :assessment_tools
+  resources :fa_criterias do |fa|
+    resources :descriptive_indicators do |desc|
+      resources :assessment_tools
     end
   end
-  map.resources :observations do |obs|
-    obs.resources :descriptive_indicators do |desc|
-      desc.resources :assessment_tools
+  resources :observations do |obs|
+    resources :descriptive_indicators do |desc|
+      resources :assessment_tools
     end
   end
-  map.resources :observation_groups,:member=>{:new_observation=>[:get,:post],:create_observation=>[:get,:post],:edit_observation=>[:get,:post],:update_observation=>[:get,:post],:destroy_observation=>[:post],:reorder=>[:get,:post]},:collection=>{:assign_courses=>[:get,:post],:set_observation_group=>[:get,:post]}
-  map.resources :cce_weightages,:member=>{:assign_courses=>[:get,:post]},:collection=>{:assign_weightages=>[:get,:post]}
-  map.resources :cce_grade_sets, :member=>{:new_grade=>[:get,:post],:edit_grade=>[:get,:post],:update_grade=>[:get,:post],:destroy_grade=>[:post]}
+  resources :observation_groups,:member=>{:new_observation=>[:get,:post],:create_observation=>[:get,:post],:edit_observation=>[:get,:post],:update_observation=>[:get,:post],:destroy_observation=>[:post],:reorder=>[:get,:post]},:collection=>{:assign_courses=>[:get,:post],:set_observation_group=>[:get,:post]}
+  resources :cce_weightages,:member=>{:assign_courses=>[:get,:post]},:collection=>{:assign_weightages=>[:get,:post]}
+  resources :cce_grade_sets, :member=>{:new_grade=>[:get,:post],:edit_grade=>[:get,:post],:update_grade=>[:get,:post],:destroy_grade=>[:post]}
 
-  map.feed 'courses/manage_course', :controller => 'courses' ,:action=>'manage_course'
-  map.feed 'courses/manage_batches', :controller => 'courses' ,:action=>'manage_batches'
-  map.resources :courses, :has_many => :batches, :collection => {:grouped_batches=>[:get,:post],:create_batch_group=>[:get,:post],:edit_batch_group=>[:get,:post],:update_batch_group=>[:get,:post],:delete_batch_group=>[:get,:post],:assign_subject_amount => [:get,:post],:edit_subject_amount => [:get,:post],:destroy_subject_amount => [:get,:post]}
+  # feed 'courses/manage_course', :controller => 'courses' ,:action=>'manage_course'
+  # feed 'courses/manage_batches', :controller => 'courses' ,:action=>'manage_batches'
+  resources :courses, :has_many => :batches, :collection => {:grouped_batches=>[:get,:post],:create_batch_group=>[:get,:post],:edit_batch_group=>[:get,:post],:update_batch_group=>[:get,:post],:delete_batch_group=>[:get,:post],:assign_subject_amount => [:get,:post],:edit_subject_amount => [:get,:post],:destroy_subject_amount => [:get,:post]}
 
-  map.resources :batches, :collection=>{:batches_ajax=>[:get]} do |batch|
-    batch.resources :exam_groups
+  resources :batches, :collection=>{:batches_ajax=>[:get]} do |batch|
+    resources :exam_groups
     #batch.resources :additional_exam_groups
-    batch.resources :elective_groups, :as => :electives
+    resources :elective_groups, :as => :electives
   end
 
-  map.resources :exam_groups do |exam_group|
-    exam_group.resources :exams, :member => { :save_scores => :post }
+  resources :exam_groups do |exam_group|
+    resources :exams, :member => { :save_scores => :post }
   end
 
-  #  map.resources :additional_exam_groups do |additional_exam_group|
+  #  resources :additional_exam_groups do |additional_exam_group|
   #    additional_exam_group.resources :additional_exams , :member => { :save_additional_scores => :post }
   #  end
 
-  map.resources :timetables do |timetable|
-    timetable.resources :timetable_entries
+  resources :timetables do |timetable|
+    resources :timetable_entries
   end
-  map.root :controller => 'user', :action => 'login'
+  root :controller => 'user', :action => 'login'
 
-  map.fa_scores 'assessment_scores/exam/:exam_id/fa_group/:fa_group_id', :controller=>'assessment_scores',:action=>'fa_scores'
-  map.observation_scores 'assessment_scores/batch/:batch_id/observation_group/:observation_group_id', :controller=>'assessment_scores',:action=>'observation_scores'
-  map.scheduled_task 'scheduled_jobs/:job_object/:job_type',:controller => "scheduled_jobs",:action => "index"
-  map.scheduled_task_object 'scheduled_jobs/:job_object',:controller => "scheduled_jobs",:action => "index"
+  # fa_scores 'assessment_scores/exam/:exam_id/fa_group/:fa_group_id', :controller=>'assessment_scores',:action=>'fa_scores'
+  # observation_scores 'assessment_scores/batch/:batch_id/observation_group/:observation_group_id', :controller=>'assessment_scores',:action=>'observation_scores'
+  # scheduled_task 'scheduled_jobs/:job_object/:job_type',:controller => "scheduled_jobs",:action => "index"
+  # scheduled_task_object 'scheduled_jobs/:job_object',:controller => "scheduled_jobs",:action => "index"
 
 
-  #map.connect 'parts/:number', :controller => 'inventory', :action => 'sho
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action'
-  map.connect ':controller/:action/:id/:id2'
-  map.connect ':controller/:action/:id.:format'
+  #connect 'parts/:number', :controller => 'inventory', :action => 'sho
+  # connect ':controller/:action/:id'
+  # connect ':controller/:action'
+  # connect ':controller/:action/:id/:id2'
+  # connect ':controller/:action/:id.:format'
 end
